a67175d17828a023866a869aea5df3e45e8d8038
hongpei100 <p1n2z3q4a5@gmail.com>
2023-03-01 23:08:09 +0800

    Feat: different levels of millions function

---
+++ traces/my-trace-sorting-cmp-1000000.cmd
@@ -0,0 +1,6 @@
+# Test of my merge sort v.s. Linux sort
+option fail 0
+option malloc 0
+new
+ih RAND 1000000
+sort
---
+++ traces/my-trace-sorting-cmp-10000000.cmd
@@ -0,0 +1,6 @@
+# Test of my merge sort v.s. Linux sort
+option fail 0
+option malloc 0
+new
+ih RAND 10000000
+sort
